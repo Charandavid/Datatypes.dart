d0f8c1d72d4aae8b8c70912779bfc18b008d3cd1
Charandavid <74548240+Charandavid@users.noreply.github.com>
2023-10-19 15:45:05 +0300

    Add files via upload

---
+++ Proj 1/hello.dart
@@ -0,0 +1,33 @@
+void main() {
+  // Integer data type
+  int age = 15;
+  print("Age: $age");
+
+  // Double data type
+  double temperature = 18.5;
+  print("Temperature: $temperature");
+
+  // String data type
+  String name = "David Charan";
+  print("Name: $name");
+
+  // Boolean data type
+  bool isStudent = true;
+  print("Is Student: $isStudent");
+
+  // List data type
+  List<String> fruits = ["Mango", "Pinnaple", "Orange"];
+  print("Fruits: $fruits");
+
+  // Map data type
+  Map<String, int> scores = {"Hung": 85, "Keth": 60, "Meng": 56};
+  print("Scores: $scores");
+
+  // Dynamic data type
+  var dynamicVariable = "This can be any type";
+  print("Dynamic Variable: $dynamicVariable");
+
+  // Type inference
+  var dynamicList = [1, 2, 3, 4, 5];
+  print("Dynamic List: $dynamicList");
+}
